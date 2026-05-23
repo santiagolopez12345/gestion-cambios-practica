f26b25e1188a7d45797762ef45f33aac1afa3cb4
santiagolopez12345 <santiagolopezb.2008@gmail.com>
2026-05-23 09:47:41 -0500

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,1 +1,14 @@
+<?php
 
+$conn = mysqli_connect("localhost","root","","biblioteca");
+
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+
+$query = "INSERT INTO usuarios VALUES ('$user','$pass')";
+
+mysqli_query($conn,$query);
+
+echo "Usuario registrado";
+
+?>
